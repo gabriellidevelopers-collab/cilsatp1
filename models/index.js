3212ddfc811034c376f67953e302cc8c57a5dc1c
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-07-19 17:38:02 -0300

    Create models/index.js to export all models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Tutor = require('./Tutor');
+const Entrenador = require('./Entrenador');
+const Categoria = require('./Categoria');
+const Jugador = require('./Jugador');
+const Asistencia = require('./Asistencia');
+const Cuota = require('./Cuota');
+
+module.exports = {
+  Tutor,
+  Entrenador,
+  Categoria,
+  Jugador,
+  Asistencia,
+  Cuota
+};
